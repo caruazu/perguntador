cef6aa54e6fcc3f7328a8b0b0a5d32e601425a85
caruazu <gustavocaruazu@gmail.com>
2024-07-22 18:39:50 -0300

    esboço do projeto

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# perguntador
+# Perguntador
 Um aplicativo para gerenciar pequenos testes múltipla escolha
+
+## Dependências
+1. Ubuntu 22 (jammy)
+2. linux kernel 5.15
+
+O restante das dependências são gerenciadas pelo Maven.
---
+++ src/test/java/com/portifolio/perguntador/PerguntadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.perguntador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerguntadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
